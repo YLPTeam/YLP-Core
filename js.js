a3b092f8f4334c0417fc29a316adc7493c583951
Saccy <mc.saccy@gmail.com>
2017-02-15 18:13:45 +0000

    Add files via upload

--- js.js
+++ js.js
@@ -2,4 +2,10 @@ $(document).ready(function() {
     $("#tab_content_downloads").hide();
     $("#tab_content_developers").hide();
     $("#tab_content_about").hide();
+    
+    $('.tabs').click(function(e) {
+        e.preventDefault();
+        $(this).addClass('active').siblings().removeClass('active');
+        $($(this).attr('href')).slideDown(400).siblings().slideUp(400);
+    });
 });
